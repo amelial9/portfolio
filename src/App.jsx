79ed7f096656503b12a2c0eb850a28e0c6c2dfd1
Amelia <amelial9@uw.edu>
2026-04-06 16:17:49 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import './App.css'
 
 import Home from './components/Home'
 import Research from './components/Research'
 import Aisiem from './components/Aisiem'
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/research" element={<Research />} />
-      <Route path="/aisiem" element={<Aisiem />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/research" element={<Research />} />
+        <Route path="/aisiem" element={<Aisiem />} />
+      </Routes>
+    </>
   )
 }
 
